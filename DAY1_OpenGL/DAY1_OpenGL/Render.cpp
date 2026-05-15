19e1d032c344bde187e0b41a3ce2f6fcb8f345e1
恒振 张 <1776652264@qq.com>
2026-05-15 16:06:08 +0800

    DAY5: 添加顶点颜色并实现彩色三角形

--- DAY1_OpenGL/DAY1_OpenGL/Render.cpp
+++ DAY1_OpenGL/DAY1_OpenGL/Render.cpp
@@ -4,26 +4,30 @@
 
 float vertices[] =
 {
-	-0.5f , -0.5f , 0.0f,
-	 0.5f , -0.5f , 0.0f,
-	 0.0f , 0.5f ,0.0f
+	-0.5f , -0.5f , 0.0f,     1.0f , 0.0f , 0.0f ,
+	 0.5f , -0.5f , 0.0f,     0.0f , 1.0f , 0.0f ,
+	 0.0f , 0.5f ,0.0f,       0.0f , 0.0f , 1.0f
 
 };
 
-const char* VertexShaderSource =
+const char* VertexShaderSource =                       //顶点着色器源码
 "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
 "    gl_Position = vec4(aPos.x , aPos.y , aPos.z , 1.0);\n"
+"	ourColor = aColor;\n"
 "}";
 
-const char* fragmentShaderSource =
+const char* fragmentShaderSource =               //片段着色器源码
 "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f , 0.5f , 0.2f , 1.0f);\n"
+"   FragColor = vec4(ourColor, 1.0);\n"
 "}";
 
 
@@ -87,8 +91,10 @@ void Render::initTriangle()
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);      //写入数据  （写入区域，写入大小，写入对象，相关操作）
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);   //定义顶点属性的布局（索引属性，vec数量，分量类型，数据是否初始化，分量步长，偏移量）
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);   //定义顶点属性的布局（索引属性，vec数量，分量类型，数据是否初始化，分量步长，偏移量）
 	glEnableVertexAttribArray(0);          //启用索引
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));   //定义顶点属性的布局（索引属性，vec数量，分量类型，数据是否初始化，分量步长，偏移量）
+	glEnableVertexAttribArray(1);          //启用索引
 }
 
 void Render::clear(float r, float g, float b)  //清屏颜色设置
